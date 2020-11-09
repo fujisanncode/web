c5ed04bdb3dbaf0f9a4c80cf81ff08d7877fd887
fujisanncode <812542982@qq.com>
2020-11-10 01:17:37 +0800

    完善动态路由

--- src/components/reader/markdown/markdown.vue
+++ src/components/reader/markdown/markdown.vue
@@ -1,35 +1,43 @@
 <template>
-  <div class="order">
-    <h5>富文本编辑器</h5>
-    <div id="vditor"></div>
-  </div>
+  <v-main>
+    <v-card class="pa-6">
+      <v-row>
+        <v-btn class="ma-1" :text="true">富文本编辑器</v-btn>
+        <v-btn class="ma-1" @click="submit">提交</v-btn>
+      </v-row>
+      <v-row>
+        <v-col>
+          <div id="vditor"></div>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-main>
 </template>
 
 <script>
 import Vditor from "vditor"
 import "vditor/dist/index.css"
+
 export default {
-  data(){
-    return{
-      contentEditor:""
+  data() {
+    return {
+      contentEditor: ""
     }
   },
-  mounted(){
-    this.contentEditor = new Vditor("vditor",{
-      height:360,
-      toolbarConfig:{
-        pin:true
+  mounted() {
+    this.contentEditor = new Vditor("vditor", {
+      height: 600,
+      toolbarConfig: {
+        pin: true
       },
-      cache:{
-        enable:false
+      cache: {
+        enable: false
       },
-      after:()=>{
+      after: () => {
         this.contentEditor.setValue("hello,Vditor+Vue!")
       }
     })
   },
-  methods:{
-
-  }
+  methods: {}
 }
 </script>
